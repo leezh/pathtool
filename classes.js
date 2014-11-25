7b57bbff48039760eb9d4ce9ef86fe6931bf638a
Lee Zher Huei <lee.zh.92@gmail.com>
2014-11-25 23:11:28 +0000

    Finished adding class skills

--- classes.js
+++ classes.js
@@ -32,7 +32,16 @@ addData(ref.classes, 'bard', {
         'disguise',
         'escapeArtist',
         'intimidate',
+        'knowArcana',
+        'knowDungeoneering',
+        'knowEngineering',
+        'knowGeography',
+        'knowHistory',
+        'knowLocal',
         'knowNature',
+        'knowNobility',
+        'knowPlanes',
+        'knowReligion',
         'linguistics',
         'perception',
         'perform',
@@ -47,18 +56,136 @@ addData(ref.classes, 'bard', {
 
 addData(ref.classes, 'cleric', {
     'name': 'Cleric',
-    'hitDie': 12,
-    'ranks': 4,
+    'hitDie': 8,
+    'ranks': 2,
+    'rankAbility': 'int',
+    'classSkills': [
+        'appraise',
+        'climb',
+        'craft',
+        'diplomacy',
+        'heal',
+        'knowArcana',
+        'knowHistory',
+        'knowNobility',
+        'knowPlanes',
+        'knowReligion',
+        'knowNature',
+        'linguistics',
+        'profession',
+        'senseMotive',
+        'spellcraft',
+    ],
 });
 
 addData(ref.classes, 'druid', {
     'name': 'Druid',
-    'hitDie': 12,
+    'hitDie': 8,
     'ranks': 4,
+    'rankAbility': 'int',
+    'classSkills': [
+        'climb',
+        'craft',
+        'fly',
+        'handleAnimal',
+        'heal',
+        'knowGeography',
+        'knowNature',
+        'perception',
+        'profession',
+        'ride',
+        'spellcraft',
+        'survival',
+        'swim',
+    ],
 });
 
 addData(ref.classes, 'fighter', {
     'name': 'Fighter',
+    'hitDie': 10,
+    'ranks': 2,
+    'rankAbility': 'int',
+    'classSkills': [
+        'climb',
+        'craft',
+        'handleAnimal',
+        'intimidate',
+        'knowDungeoneering',
+        'knowEngineering',
+        'profession',
+        'ride',
+        'survival',
+        'swim',
+    ],
+});
+
+addData(ref.classes, 'monk', {
+    'name': 'Monk',
+    'hitDie': 12,
+    'ranks': 4,
+    'rankAbility': 'int',
+    'classSkills': [
+        'acrobatics',
+        'climb',
+        'craft',
+        'escapeArtist',
+        'intimidate',
+        'knowHistory',
+        'knowReligion',
+        'perception',
+        'perform',
+        'profession',
+        'senseMotive',
+        'stealth',
+        'swim',
+    ],
+});
+
+addData(ref.classes, 'paladin', {
+    'name': 'Paladin',
+    'hitDie': 10,
+    'ranks': 2,
+    'rankAbility': 'int',
+    'classSkills': [
+        'craft',
+        'diplomacy',
+        'handleAnimal',
+        'heal',
+        'knowNobility',
+        'knowReligion',
+        'profession',
+        'ride',
+        'senseMotive',
+        'spellcraft',
+    ],
+});
+
+addData(ref.classes, 'ranger', {
+    'name': 'Ranger',
+    'hitDie': 10,
+    'ranks': 6,
+    'rankAbility': 'int',
+    'classSkills': [
+        'climb',
+        'craft',
+        'handleAnimal',
+        'heal',
+        'intimidate',
+        'knowDungeoneering',
+        'knowGeography',
+        'knowNature',
+        'perception',
+        'profession',
+        'ride',
+        'spellcraft',
+        'stealth',
+        'survival',
+        'swim',
+    ],
+});
+
+addData(ref.classes, 'rogue', {
+    'name': 'Rogue',
     'hitDie': 12,
     'ranks': 4,
     'rankAbility': 'int',
@@ -69,55 +196,60 @@ addData(ref.classes, 'fighter', {
         'climb',
         'craft',
         'diplomacy',
+        'disableDevice',
         'disguise',
         'escapeArtist',
         'intimidate',
-        'knowNature',
+        'knowDungeoneering',
+        'knowLocal',
         'linguistics',
         'perception',
         'perform',
         'profession',
         'senseMotive',
         'sleightOfHand',
-        'spellcraft',
         'stealth',
+        'swim',
         'useMagic',
     ],
 });
 
-addData(ref.classes, 'monk', {
-    'name': 'Monk',
-    'hitDie': 12,
-    'ranks': 4,
-});
-
-addData(ref.classes, 'paladin', {
-    'name': 'Paladin',
-    'hitDie': 12,
-    'ranks': 4,
-});
-
-addData(ref.classes, 'ranger', {
-    'name': 'Ranger',
-    'hitDie': 12,
-    'ranks': 4,
-});
-
-addData(ref.classes, 'rouge', {
-    'name': 'Rouge',
-    'hitDie': 12,
-    'ranks': 4,
-});
-
 addData(ref.classes, 'sorcerer', {
     'name': 'Sorcerer',
-    'hitDie': 12,
-    'ranks': 4,
+    'hitDie': 6,
+    'ranks': 2,
+    'rankAbility': 'int',
+    'classSkills': [
+        'appraise',
+        'bluff',
+        'craft',
+        'fly',
+        'intimidate',
+        'knowArcana',
+        'profession',
+        'spellcraft',
+        'useMagic',
+    ],
 });
 
 addData(ref.classes, 'wizard', {
     'name': 'Wizard',
-    'hitDie': 12,
-    'ranks': 4,
+    'hitDie': 6,
+    'ranks': 2,
+    'rankAbility': 'int',
+    'classSkills': [
+        'appraise',
+        'craft',
+        'fly',
+        'knowArcana',
+        'knowHistory',
+        'knowNobility',
+        'knowPlanes',
+        'knowReligion',
+        'knowNature',
+        'linguistics',
+        'profession',
+        'spellcraft',
+    ],
 });
 
